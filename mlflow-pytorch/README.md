fc8e97936129bbe1c468c191007813181035f660
Chris Wallace <chris@bitcollider.io>
2020-03-20 14:08:31 +0000

    Update mlflow-pytorch with mlflow logging

---
+++ mlflow-pytorch/README.md
@@ -0,0 +1,13 @@
+# mlflow pytorch
+
+Demonstrating mlflow tracking on a tiny pytorch script.
+
+Start a run with
+
+```bash
+python main.py \
+    --lr = 0.001    # learning rate
+    --hidden = 100  # hidden dimension
+```
+
+and start the interface with `mlflow ui`.
--- mlflow-pytorch/main.py
+++ mlflow-pytorch/main.py
@@ -1,10 +1,18 @@
 import torch
+import mlflow
+import mlflow.pytorch
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--lr', type=float, default=1e-3)
+parser.add_argument('--hidden', type=int, default=100)
+args = parser.parse_args()
 
 # generate some fake data
 
 batch_size = 64
 input_dim = 1000
-hidden_dim = 100
+hidden_dim = args.hidden
 output_dim = 10
 
 x = torch.randn(batch_size, input_dim)
@@ -20,23 +28,35 @@ model = torch.nn.Sequential(
 )
 
 mse = torch.nn.MSELoss()
-
+l1 = torch.nn.L1Loss()
 
 # define an optimizer
 
-lr = 1e-4
-opt = torch.optim.SGD(model.parameters(), lr=lr)
+opt = torch.optim.SGD(model.parameters(), lr=args.lr)
 
 
 # run a training loop
 
-for epoch in range(100):
-    preds = model(x)
-    loss = mse(preds, y)
+with mlflow.start_run():
+
+    mlflow.log_params({
+        'lr': args.lr,
+        'hidden': args.hidden
+    })
+
+    for epoch in range(100):
+        preds = model(x)
+        loss = mse(preds, y)
+        l1_loss = l1(preds, y)        
+ 
+        mlflow.log_metrics({
+            'loss': loss.item(),
+            'squared loss': loss.item()**2,
+            'l1 loss': l1_loss.item()
+        }, step=epoch)
+        
+        opt.zero_grad()
+        loss.backward()
+        opt.step() 
     
-    print('epoch: {}, loss: {}'.format(epoch, loss))
-    
-    opt.zero_grad()
-    loss.backward()
-    opt.step()
-    
+    mlflow.pytorch.log_model(model ,'model')
